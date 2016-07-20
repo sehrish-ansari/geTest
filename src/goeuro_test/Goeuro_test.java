5d91d615d37ddd606aa24b17f524635dd434a0b9
Ansari <sehrish.ansari11@gmail.com>
2016-07-20 23:50:10 +0500

    v1.2

--- src/goeuro_test/Goeuro_test.java
+++ src/goeuro_test/Goeuro_test.java
@@ -10,11 +10,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.json.JSONArray;
- 
 import org.json.JSONObject;
  
 /**
